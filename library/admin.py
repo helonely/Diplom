c5b6fded308cb2973074db924a82fe3bd3b17ec7
helonely <idravil1211@gmail.com>
2024-11-16 00:53:59 +0500

    added AuthorAdmin and BookAdmin

--- library/admin.py
+++ library/admin.py
@@ -1,2 +1,17 @@
 from django.contrib import admin
 
+from library.models import Author, Book
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'availability', 'author', 'genre')
+    list_filter = ('name', 'availability', 'author', 'genre')
+    search_fields = ('name', 'availability', 'author', 'genre')
