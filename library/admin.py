12bd1ccf8730a26db54b4e437c4a6721819a8901
helonely <idravil1211@gmail.com>
2024-11-16 02:33:43 +0500

    added id and LoanAdmin

--- library/admin.py
+++ library/admin.py
@@ -1,17 +1,24 @@
 from django.contrib import admin
 
-from library.models import Author, Book
+from library.models import Author, Book, Loan
 
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    list_filter = ('name',)
-    search_fields = ('name',)
+    list_display = ('id', 'name',)
+    list_filter = ('id', 'name',)
+    search_fields = ('id', 'name',)
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('name', 'availability', 'author', 'genre')
-    list_filter = ('name', 'availability', 'author', 'genre')
-    search_fields = ('name', 'availability', 'author', 'genre')
+    list_display = ('id', 'name', 'availability', 'author', 'genre')
+    list_filter = ('id', 'name', 'availability', 'author', 'genre')
+    search_fields = ('id', 'name', 'availability', 'author', 'genre')
+
+
+@admin.register(Loan)
+class LoanAdmin(admin.ModelAdmin):
+    list_display = ('id', 'book', 'user', 'issue_date', 'return_date')
+    list_filter = ('id', 'book', 'user', 'issue_date', 'return_date')
+    search_fields = ('id', 'book', 'user', 'issue_date', 'return_date')
