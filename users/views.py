8b915c86191c0ac46c84b3b3fe113262e6c84684
helonely <idravil1211@gmail.com>
2024-11-16 00:59:22 +0500

    added views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView
+)
+from library.permissions import IsModerator
+from users.models import User
+from users.serializers import UserSerializer
 
-# Create your views here.
+
+class UserCreateApiView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsModerator]
+
+
+class UserListApiView(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsModerator]
+
+
+class UserRetrieveApiView(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserUpdateApiView(UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsModerator]
+
+
+class UserDestroyApiView(DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsModerator]
