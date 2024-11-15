383afc094bf168cdd3f3858c9737ed53ead65813
helonely <idravil1211@gmail.com>
2024-11-16 00:59:18 +0500

    added views

--- library/views.py
+++ library/views.py
@@ -1,3 +1,79 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView
+)
+from library.models import Author, Book
+from library.paginators import BookPaginator, AuthorPaginator
+from library.permissions import IsModerator
+from library.serializers import BookSerializer, AuthorSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+"""CRUD for authors"""
+
+
+class AuthorCreateApiView(CreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [IsModerator,]
+
+
+class AuthorListApiView(ListAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    pagination_class = AuthorPaginator
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name',]
+
+
+class AuthorRetrieveApiView(RetrieveAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class AuthorUpdateApiView(UpdateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [IsModerator]
+
+
+class AuthorDestroyApiView(DestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [IsModerator]
+
+
+"""CRUD for books"""
+
+
+class BookCreateApiView(CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsModerator]
+
+
+class BookListApiView(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    pagination_class = BookPaginator
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'genre', 'user', 'availability', 'author']
+
+
+class BookRetrieveApiView(RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookUpdateApiView(UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsModerator]
+
+
+class BookDestroyApiView(DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsModerator]
